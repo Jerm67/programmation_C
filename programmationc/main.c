44c94c1f2b37ee78cef9f7d1fd66d6566893993c
Jerm67 <j.maggiotti@ludus-academie.com>
2020-11-18 12:19:48 +0100

    no message

--- programmationc/main.c
+++ programmationc/main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main() //
 {
-    printf("Hello world!\n");
+
+    //unsigned ; non signé ex : unsigned short sn_entier
+    //integer
+    short sn_entier;
+    int n_entier;
+    long l_entier;
+
+    printf("Taille en octet du short : %d\n",sizeof (sn_entier));
+    printf("Taille en octet d entier : %d\n",sizeof (n_entier));
+    printf("Taille en octet du long: %d\n",sizeof (l_entier));
+
+    //reel
+    //unsigned ; non signé ex : unsigned float flt_reel
+    float flt_reel;
+    double dbl_reel;
+    long double ldbl_reel;
+
+    printf("Taille en octet du float : %d\n",sizeof (flt_reel));
+    printf("Taille en octet du double : %d\n",sizeof (dbl_reel));
+    printf("Taille en octet du long double: %d\n",sizeof (ldbl_reel));
+
+    //caractère
+    char c_char;
+    printf("Taille en octet du caractere: %d\n",sizeof (c_char));
+
+
+
+
     return 0;
 }
